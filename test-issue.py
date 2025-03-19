5d2a8a4937223fb27dcb135ee309972ecb7f2dd0
Fuzail Ahmad <fuzailahmad2605@gmail.com>
2025-03-19 08:20:37 +0530

    added intentional error

--- test-issue.py
+++ test-issue.py
@@ -1,2 +1,10 @@
 
 # import hashlib
+
+def faulty_exception_handling():
+    try:
+        result = 10 / 0 
+    except ValueError:  
+        print("Handled error incorrectly")
+
+faulty_exception_handling()
