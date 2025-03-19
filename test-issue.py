13b58f741cdbe6f72719d256dd0ae122101b37fe
Fuzail Ahmad <fuzailahmad2605@gmail.com>
2025-03-19 05:38:21 +0530

    added intentional error for pipeline checking

--- test-issue.py
+++ test-issue.py
@@ -1,12 +1,12 @@
 
-import hashlib
+# import hashlib
 
-def weak_hashing(password):
-    hash_value = hashlib.md5(password.encode()).hexdigest() 
-    return hash_value
+# def weak_hashing(password):
+#     hash_value = hashlib.md5(password.encode()).hexdigest() 
+#     return hash_value
 
-def division_by_zero(user_input):
-    return 10 / int(user_input)
+# def division_by_zero(user_input):
+#     return 10 / int(user_input)
 
-division_by_zero("0")
-print(weak_hashing("password123"))
+# division_by_zero("0")
+# print(weak_hashing("password123"))
